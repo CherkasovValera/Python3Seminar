8398da5f0b2dc67a59c9c4b77cef4d5b8df9f697
Valeriy <cherkasov77@mail.ru>
2022-10-07 01:04:50 +0400

    Sem3num5

--- Seminar3_4.py
+++ Seminar3_4.py
@@ -8,11 +8,14 @@
 import math
 b = abs(int(input('Введите число заначений:')))
 kod = []
-print(b,'->',end='')
-while b>= 1:
-    n=b%2
-    b=b//2
-    kod.append(n)
+print(b,' -> ',end='')
+if b==0:
+    print(b,end='')
+else:
+        while b>= 1:
+            n=b%2
+            b=b//2
+            kod.append(n)
 kod.reverse()
 print(*kod,sep='')
 
